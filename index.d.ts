ded18164e07edc6397b90007415d04cc5a98a089
crustypluto19 <evanchristopher1907@gmail.com>
2024-11-18 18:51:19 +0100

    Refactor BloomFilter declaration to use interface and class structure

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,25 @@
-declare module 'my-forked-bloomfilter' {
-    export class BloomFilter {
-      m: number;
-      k: number;
-      buckets: Int32Array | number[];
-      _locations: Uint8Array | Uint16Array | Uint32Array | any[];
-  
-      constructor(m: number | number[], k: number);
-  
-      locations(v: string): number[];
-      add(v: string): void;
-      test(v: string): boolean;
-      size(): number;
-    }
+export interface BloomFilter {
+  m: number;
+  k: number;
+  buckets: Int32Array | number[];
+  _locations: Uint8Array | Uint16Array | Uint32Array | any[];
+
+  locations(v: string): number[];
+  add(v: string): void;
+  test(v: string): boolean;
+  size(): number;
+}
+
+export declare class BloomFilter implements BloomFilter {
+  m: number;
+  k: number;
+  buckets: Int32Array | number[];
+  _locations: Uint8Array | Uint16Array | Uint32Array | any[];
+
+  constructor(m: number | number[], k: number);
+
+  locations(v: string): number[];
+  add(v: string): void;
+  test(v: string): boolean;
+  size(): number;
 }
